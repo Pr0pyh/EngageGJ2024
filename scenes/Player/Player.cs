6d1d48d83b1f19710d73d36a614e1934cf2cd3f0
VukAmidzic <vuk.amidzic@gmail.com>
2024-03-29 23:07:35 +0100

    enemy movement + detection

--- scenes/Player/Player.cs
+++ scenes/Player/Player.cs
@@ -4,72 +4,72 @@ using System.Data;
 
 public partial class Player : CharacterBody3D
 {
-    //enum deklaracija
-    enum STATE {
-        MOVE
-    }
-    STATE state;
+	//enum deklaracija
+	enum STATE {
+		MOVE
+	}
+	STATE state;
 
-    //export varijable
-    [Export]
-    public float eSpeed;
-    [Export]
-    public float eSensitivity;
+	//export varijable
+	[Export]
+	public float eSpeed;
+	[Export]
+	public float eSensitivity;
 
-    //Node access varijable
-    Camera3D nCamera;
-    //private funkcije definicije
-    private void move()
-    {
-        Godot.Vector3 moveVector = new Godot.Vector3(0.0f, 0.0f, 0.0f);
-        if(Input.IsActionPressed("up"))
-            moveVector -= nCamera.GlobalTransform.Basis.Z;
-        if(Input.IsActionPressed("down"))
-            moveVector += nCamera.GlobalTransform.Basis.Z;
-        if(Input.IsActionPressed("right"))
-            moveVector += nCamera.GlobalTransform.Basis.X;
-        if(Input.IsActionPressed("left"))
-            moveVector -= nCamera.GlobalTransform.Basis.X;
-        moveVector = new Vector3(moveVector.X, 0.0f, moveVector.Z);
-        Velocity = eSpeed*moveVector;
-        MoveAndSlide();
-    }
+	//Node access varijable
+	Camera3D nCamera;
+	//private funkcije definicije
+	private void move()
+	{
+		Godot.Vector3 moveVector = new Godot.Vector3(0.0f, 0.0f, 0.0f);
+		if(Input.IsActionPressed("up"))
+			moveVector -= nCamera.GlobalTransform.Basis.Z;
+		if(Input.IsActionPressed("down"))
+			moveVector += nCamera.GlobalTransform.Basis.Z;
+		if(Input.IsActionPressed("right"))
+			moveVector += nCamera.GlobalTransform.Basis.X;
+		if(Input.IsActionPressed("left"))
+			moveVector -= nCamera.GlobalTransform.Basis.X;
+		moveVector = new Vector3(moveVector.X, 0.0f, moveVector.Z);
+		Velocity = eSpeed*moveVector;
+		MoveAndSlide();
+	}
 
-    public void quitInput()
-    {
-        if(Input.IsActionPressed("exit"))
-            GetTree().Quit();
-    }
+	public void quitInput()
+	{
+		if(Input.IsActionPressed("exit"))
+			GetTree().Quit();
+	}
 
-    private void nodeInitialize()
-    {
-        nCamera = GetNode<Node3D>("Head").GetNode<Camera3D>("Camera3D");
-    }
+	private void nodeInitialize()
+	{
+		nCamera = GetNode<Node3D>("Head").GetNode<Camera3D>("Camera3D");
+	}
 
-    //godot functions
-    public override void _Input(InputEvent @event)
-    {
-        if(@event is InputEventMouseMotion mouseMotion)
-        {
-            nCamera.RotateX(Mathf.DegToRad(mouseMotion.Relative.Y*eSensitivity*-1));
+	//godot functions
+	public override void _Input(InputEvent @event)
+	{
+		if(@event is InputEventMouseMotion mouseMotion)
+		{
+			nCamera.RotateX(Mathf.DegToRad(mouseMotion.Relative.Y*eSensitivity*-1));
 			nCamera.RotationDegrees = new Vector3(Mathf.Clamp(nCamera.RotationDegrees.X, -75.0f, 75.0f), 0.0f, 0.0f);
-            this.RotateY(Mathf.DegToRad(mouseMotion.Relative.X*eSensitivity*-1));
-        }
-    }
-    public override void _Ready()
-    {
-        nodeInitialize();
-        Input.MouseMode = Input.MouseModeEnum.Captured;
-    }
+			this.RotateY(Mathf.DegToRad(mouseMotion.Relative.X*eSensitivity*-1));
+		}
+	}
+	public override void _Ready()
+	{
+		nodeInitialize();
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        switch(state)
-        {
-            case STATE.MOVE:
-                move();
-                quitInput();
-                break;
-        }
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		switch(state)
+		{
+			case STATE.MOVE:
+				move();
+				quitInput();
+				break;
+		}
+	}
 };
--- scenes/TestEnemy/TestEnemy.cs
+++ scenes/TestEnemy/TestEnemy.cs
@@ -2,14 +2,65 @@ using Godot;
 using System;
 
 public partial class TestEnemy : CharacterBody3D
-{
-    public void damage()
-    {
-        GD.Print("pozovi");
-    }
+{	
+	enum STATE {
+		IDLE,
+		FOLLOW
+	}
+	
+	STATE state;
+	Player player;
+	
+	public override void _Ready()
+	{
+		state = STATE.FOLLOW;
+		player = GetParent().GetNode<Player>("Player");
+		GD.Print(player == null);
+	}
+	
+	public override void _Process(double delta)
+	{
+		switch (this.state) 
+		{
+			case STATE.IDLE :
+			{
+				GD.Print("IDLE STATE");
+				break;	
+			} 
+			case STATE.FOLLOW :
+			{
+				followPlayer((float)delta);
+				break;	
+			}
+		}
+	}
+	
+		
+	public void damage()
+	{
+		GD.Print("pozovi");
+	}
 
-    public void show()
-    {
-        GD.Print("pokazi");
-    }
+	public void show()
+	{
+		GD.Print("pokazi");
+	}
+	
+	private void followPlayer(float delta)
+	{
+		Vector3 newPos = this
+						.GlobalPosition
+						.MoveToward(player.GlobalPosition, (float)delta);
+		this.GlobalPosition = newPos;
+	}
+	
+	private void _on_area_3d_body_entered(Node3D body)
+	{
+		if (this.state != STATE.FOLLOW) this.state = STATE.FOLLOW;
+	}
+	
+	private void _on_area_3d_body_exited(Node3D body)
+	{
+		if (this.state != STATE.IDLE) this.state = STATE.IDLE;
+	}
 }
