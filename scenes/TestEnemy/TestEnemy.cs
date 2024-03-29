9d41bc3b92754d36496f8c1ee1b4b36fe374b257
VukAmidzic <vuk.amidzic@gmail.com>
2024-03-30 00:31:35 +0100

    additional enemy behaviour

--- scenes/TestEnemy/TestEnemy.cs
+++ scenes/TestEnemy/TestEnemy.cs
@@ -18,13 +18,14 @@ public partial class TestEnemy : CharacterBody3D
 		GD.Print(player == null);
 	}
 	
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
 		switch (this.state) 
 		{
 			case STATE.IDLE :
 			{
 				GD.Print("IDLE STATE");
+				MoveAndSlide();
 				break;	
 			} 
 			case STATE.FOLLOW :
@@ -52,6 +53,19 @@ public partial class TestEnemy : CharacterBody3D
 						.GlobalPosition
 						.MoveToward(player.GlobalPosition, (float)delta);
 		this.GlobalPosition = newPos;
+	}
+	
+	private void _on_timer_timeout()
+	{
+		if (this.state == STATE.IDLE)
+		{
+			Random rnd = new Random();
+			float randDeg = (float)rnd.NextDouble()*360.0f;
+			float randRad = randDeg * (float)Math.PI / 180.0f;
+			this.RotateY(randRad);		
+			this.Velocity = new Vector3(1.0f, 0.0f, 0.0f);
+			//this.Velocity = this.Velocity.Rotated(new Vector3(0.0f, 1.0f, 0.0f), randRad);
+		}
 	}
 	
 	private void _on_area_3d_body_entered(Node3D body)
