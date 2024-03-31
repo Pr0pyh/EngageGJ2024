f1596d6daa62609f681c164bc352e31563833015
VukAmidzic <vuk.amidzic@gmail.com>
2024-03-31 05:05:40 +0200

    added storytelling labels across levels

--- scenes/Level/Level.cs
+++ scenes/Level/Level.cs
@@ -3,21 +3,21 @@ using System;
 
 public partial class Level : Node3D
 {
-    [Export]
-    public int number;
-    [Export]
-    Finish finish;
+	[Export]
+	public int number;
+	[Export]
+	Finish finish;
 
-    public override void _Ready()
-    {
-        finish = GetNode<Finish>("Finish");
-    }
-    public void lowerNumber()
-    {
-        number--;
-        if(number <= 0)
-            finish.pristupAllow();
-    }
-    //komentar
-    
+	public override void _Ready()
+	{
+		finish = GetNode<Finish>("Finish");
+	}
+	public void lowerNumber()
+	{
+		number--;
+		if(number <= 0)
+			finish.pristupAllow();
+	}
+	//komentar
+	
 }
